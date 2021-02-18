96c49fdc711e9cff9f7dbe4b652e6fafc39a4faf
Beth Hart <errordrivendev@gmail.com>
2021-02-17 20:31:07 -0500

    eaer an footer comp

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProductList } from '../components/ProductList'
+
+export const HomePage = () => {
+    return (
+        <div>
+            <ProductList></ProductList>
+        </div>
+    )
+}
