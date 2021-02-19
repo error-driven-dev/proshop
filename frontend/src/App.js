53a320ed02993e24a7ba9244f36ea8b93ae424e9
bhart2 <bhart@collabralink.com>
2021-02-19 13:39:39 -0500

    tweak styling and add to cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import {Header} from './components/Header'
-import {Footer} from './components/Footer'
-import { Container } from 'react-bootstrap'
-import { HomePage } from './pages/HomePage'
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom'
+import React from "react"
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import { Container } from "react-bootstrap"
+import { HomePage } from "./pages/HomePage"
+import { ProductPage } from "./pages/ProductPage"
+import { Link, Route, BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   return (
     <>
-    <Header></Header>
-<main>
-  <Container> <Router>
-    <Route>
-    <HomePage></HomePage>
-    </Route>
-  </Router>
-
-  </Container>
-</main>
-<Footer></Footer>
+      <Header></Header>
+      <main>
+        <Container>
+          {" "}
+          <Router>
+            <Route path='/' exact component={HomePage}></Route>
+            <Route path='/product/:id' component={ProductPage}></Route>
+          </Router>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,38 +1,63 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { Card, Row, Col, Image, ListGroup, ListGroupItem } from 'react-bootstrap'
+import React from "react"
+import { Link } from "react-router-dom"
+import {
+  Card,
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap"
+import products from "../products"
+import { Rating } from "../components/Rating"
 
-export const Product = ({product}) => {
-    return (
-       <> <Link className='btn btn-dark my-3' To="/">Back</Link>
-        <Row>
-           <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid></Image>
-            </Col>
-            <Col md={3}>
+export const ProductPage = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id)
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/' exact>
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid rounded></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h5>{product.name}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating rating={product.rating}></Rating> {product.numReviews}{" "}
+              Reviews
+            </ListGroup.Item>
+            <ListGroupItem>{product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
             <ListGroup variant='flush'>
-                <ListGroup.Item>
-                    <h3>{product.name}</h3></ListGroup.Item>
-                    <ListGroup.Item>{product.rating} stars {product.numReviews} Reviews</ListGroup.Item>
-                    <ListGroupItem>{product.description}</ListGroupItem>
-                    </ListGroup></Col>
-                    <Col md={3}>
-                        <Card>
-                            <ListGroup>
-                                <Row>
-                                    <Col> Price: {product.price}
-                                    </Col>
-                                </Row>
-                            </ListGroup>
-                            <ListGroup>
-                                <Row>
-                                    <Col>       Status: {product.countInStock > 0 ? "Product in Stock" : "Sold Out"}
-                                    </Col>
-                                </Row>
-                     </ListGroup>
-                        </Card>
-                    </Col>
-                
-       </Row>
-
-       </>
+              <Row>
+                <Col>
+                  <ListGroup.Item>
+                    <h5>Price: ${product.price}</h5>
+                  </ListGroup.Item>
+                </Col>
+              </Row>
+            </ListGroup>
+          </Card>
+          <ListGroup>
+            <Button
+              className='btn btn-block'
+              type='button'
+              disabled={product.countInStock === 0}
+            >
+              {product.countInStock > 0 ? "Add to Cart" : "Sold Out"}
+            </Button>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  )
+}
